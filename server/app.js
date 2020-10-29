b12ad62f604932210af06572d4518805607b072f
Jame3sHalliday <quanggtangg@gmail.com>
2020-10-29 22:12:25 +0700

    Revert "#16 - connect with mlab but it's fucking lag, maybe i need to find another daas"
    This reverts commit b9a215bfb2308a673f3c69c8ceb73aa62ed54464.

--- server/app.js
+++ server/app.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
-const mongoose = require('mongoose');
 
 const app = express();
-
-mongoose.connect('mlab-url-after-you-create-it-but-mlab-is-fucking-lag/god-damn-it')
-mongoose.connect.one('open', () => {
-    console.log('connect to db');
-})
 
 app.use('/graphql', graphqlHTTP({
     schema,
